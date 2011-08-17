e5ebf56909f5d744ffcab35520f5ab77a5f9aee5
cwjacksonjr (CJ Jackson) <jackson.cwjr@gmail.com>
2011-08-17 22:17:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ambur@103 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ambur.statshape.R
+++ pkg/R/ambur.statshape.R
@@ -1,7 +1,7 @@
 ambur.statshape <-
 function(npts=50) {
 require(tcltk)
-
+require(rgdal)
 tkmessageBox(message = "Please select the GIS_stats_table_short.csv file...")
 
 
@@ -9,6 +9,13 @@ getdata <- tk_choose.files(default = "*.csv",multi = FALSE)
 
 mydata <- read.table(getdata, header=TRUE, sep=",")
 attach(mydata)
+
+#mydata$ID <- seq(0,length(mydata$Transect)-1,1)
+
+#replace characters in fields that GIS doesn't read
+colnames(mydata) <- gsub(".", "_", colnames(mydata),fixed=TRUE)
+colnames(mydata) <- gsub(" ", "", colnames(mydata),fixed=TRUE)
+
 
 dir.path <- dirname(getdata)
 setwd(dir.path)
@@ -22,64 +29,72 @@ setwd("AMBUR_gisdata")
 
 dir.create(paste(time.stamp2," ","gisdata",sep=""))
 setwd(paste(time.stamp2," ","gisdata",sep=""))
+################################################################################################################
 
-require(shapefiles)
 
-id.field <- mydata["Transect"]
-dd <- data.frame(Id=c(id.field),X=(mydata["Outer_X"]),Y=(mydata["Outer_Y"]))
+
 ddTable <- data.frame(mydata)
-ddShapefile <- convert.to.shapefile(dd, ddTable, "Transect", 1)
-write.shapefile(ddShapefile, paste("outer_pts",sep=""), arcgis=T)
-
-id.field <- mydata["Transect"]
-dd <- data.frame(Id=c(id.field),X=(mydata["Inner_X"]),Y=(mydata["Inner_Y"]))
-ddTable <- data.frame(mydata)
-ddShapefile <- convert.to.shapefile(dd, ddTable, "Transect", 1)
-write.shapefile(ddShapefile, paste("inner_pts",sep=""), arcgis=T)
-
-id.field <- mydata["Transect"]
-dd <- data.frame(Id=c(id.field),X=(mydata["Start_X"]),Y=(mydata["Start_Y"]))
-ddTable <- data.frame(mydata)
-ddShapefile <- convert.to.shapefile(dd, ddTable, "Transect", 1)
-write.shapefile(ddShapefile, paste("start_pts",sep=""), arcgis=T)
-
-id.field <- mydata["Transect"]
-dd <- data.frame(Id=c(id.field),X=(mydata["End_X"]),Y=(mydata["End_Y"]))
-ddTable <- data.frame(mydata)
-ddShapefile <- convert.to.shapefile(dd, ddTable, "Transect", 1)
-write.shapefile(ddShapefile, paste("end_pts",sep=""), arcgis=T)
-
-id.field <- mydata["Transect"]
-dd <- data.frame(Id=c(id.field),X=(mydata["Max_DateX"]),Y=(mydata["Max_DateY"]))
-ddTable <- data.frame(mydata)
-ddShapefile <- convert.to.shapefile(dd, ddTable, "Transect", 1)
-write.shapefile(ddShapefile, paste("max_date_pts",sep=""), arcgis=T)
+coordinates(ddTable) <- data.frame(x=(mydata["Outer_X"]), y=(mydata["Outer_Y"]))
+writeOGR(ddTable, ".", "outer_pts", driver="ESRI Shapefile")
 
 
-id.field <- mydata["Transect"]
-dd <- data.frame(Id=c(id.field[,],id.field[,]),X=c(mydata["Outer_X"][,],mydata["Inner_X"][,]),Y=c(mydata["Outer_Y"][,],mydata["Inner_Y"][,]))
-ddTable <- data.frame(mydata)
-ddShapefile <- convert.to.shapefile(dd, ddTable,"Transect", 3)
-write.shapefile(ddShapefile, paste("transects_envelope",sep=""), arcgis=T)
+ddTable <- data.frame(mydata) 
+coordinates(ddTable) <- data.frame(x=(mydata["Inner_X"]), y=(mydata["Inner_Y"]))
+writeOGR(ddTable, ".", "inner_pts", driver="ESRI Shapefile")
 
 
-id.field <- mydata["Transect"]
-dd <- data.frame(Id=c(id.field[,],id.field[,]),X=c(mydata["Start_X"][,],mydata["End_X"][,]),Y=c(mydata["Start_Y"][,],mydata["End_Y"][,]))
-ddTable <- data.frame(mydata)
-ddShapefile <- convert.to.shapefile(dd, ddTable,"Transect", 3)
-write.shapefile(ddShapefile, paste("transects_original",sep=""), arcgis=T)
+ddTable <- data.frame(mydata) 
+coordinates(ddTable) <- data.frame(x=(mydata["Start_X"]), y=(mydata["Start_Y"]))
+writeOGR(ddTable, ".", "start_pts", driver="ESRI Shapefile")
 
 
-id.field <- mydata["Transect"]
-dd <- data.frame(Id=c(id.field[,],id.field[,]),X=c(mydata["Min_DateX"][,],mydata["Max_DateX"][,]),Y=c(mydata["Min_DateY"][,],mydata["Max_DateY"][,]))
-ddTable <- data.frame(mydata)
-ddShapefile <- convert.to.shapefile(dd, ddTable,"Transect", 3)
-write.shapefile(ddShapefile, paste("transects_min_max_date",sep=""), arcgis=T)
+ddTable <- data.frame(mydata) 
+coordinates(ddTable) <- data.frame(x=(mydata["End_X"]), y=(mydata["End_Y"]))
+writeOGR(ddTable, ".", "end_pts", driver="ESRI Shapefile")
 
+
+ddTable <- data.frame(mydata) 
+coordinates(ddTable) <- data.frame(x=(mydata["Max_DateX"]), y=(mydata["Max_DateY"]))
+writeOGR(ddTable, ".", "max_date_pts", driver="ESRI Shapefile")
+
+
+ddTable <- data.frame(mydata) 
+coordinates(ddTable) <- data.frame(x=(mydata["Min_DateX"]), y=(mydata["Min_DateY"]))
+writeOGR(ddTable, ".", "min_date_pts", driver="ESRI Shapefile")
+
+################################################################################################################
+new_trandata <- data.frame(mydata)
+Transect.Factor <- factor(new_trandata$Transect)
+
+
+shape.final <- sapply(levels(Transect.Factor), function(x)
+list(Lines(list(Line(list(x=c(new_trandata$Start_X[new_trandata$Transect == x], new_trandata$End_X[new_trandata$Transect == x]), y=c(new_trandata$Start_Y[new_trandata$Transect == x],new_trandata$End_Y[new_trandata$Transect == x])))), ID=(as.numeric(x))))
+,simplify = TRUE)
+shape.final2 <- SpatialLines(shape.final)
+shape.final3 <- SpatialLinesDataFrame(shape.final2, new_trandata)
+writeOGR(shape.final3, ".", "original_transects", driver="ESRI Shapefile")
+
+
+shape.final <- sapply(levels(Transect.Factor), function(x)
+list(Lines(list(Line(list(x=c(new_trandata$Outer_X[new_trandata$Transect == x], new_trandata$Inner_X[new_trandata$Transect == x]), y=c(new_trandata$Outer_Y[new_trandata$Transect == x],new_trandata$Inner_Y[new_trandata$Transect == x])))), ID=(as.numeric(x))))
+,simplify = TRUE)
+shape.final2 <- SpatialLines(shape.final)
+shape.final3 <- SpatialLinesDataFrame(shape.final2, new_trandata)
+writeOGR(shape.final3, ".", "envelope_transects", driver="ESRI Shapefile")
+
+
+shape.final <- sapply(levels(Transect.Factor), function(x)
+list(Lines(list(Line(list(x=c(new_trandata$Min_DateX[new_trandata$Transect == x], new_trandata$Max_DateX[new_trandata$Transect == x]), y=c(new_trandata$Min_DateY[new_trandata$Transect == x],new_trandata$Max_DateY[new_trandata$Transect == x])))), ID=(as.numeric(x))))
+,simplify = TRUE)
+shape.final2 <- SpatialLines(shape.final)
+shape.final3 <- SpatialLinesDataFrame(shape.final2, new_trandata)
+writeOGR(shape.final3, ".", "net_min_max__date_transects", driver="ESRI Shapefile")
+
+################################################################################################################
 
 
 #tidy up and remove all objects
-#detach("package:shapefiles")
+
 detach(mydata)
 rm(list = ls())
 
