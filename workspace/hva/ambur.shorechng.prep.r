f49a4a27bf64390b864eb7b4c19cfdadd10e2e00
cwjacksonjr (CJ Jackson) <jackson.cwjr@gmail.com>
2013-09-17 05:04:02 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ambur@222 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- workspace/hva/ambur.shorechng.prep.r
+++ workspace/hva/ambur.shorechng.prep.r
@@ -1,9 +1,9 @@
 ambur.shorechng.prep <-
-function(userinput1=1) {
+function(bufferdist=150) {
 
 
 # Establish the inputs
-nothing <- userinput1
+
 
 
 require(rgdal)
@@ -43,25 +43,77 @@ Pcnt.Complete <-  75
 info <- sprintf("%d%% done", Pcnt.Complete)
 setTkProgressBar(pb, Pcnt.Complete , sprintf("AMBUR: Preparing data... (%s)", info), info)
 
-######HVA index setup
-ESI_values <- attrtable[,"EPR"]
+######HVA index setup for shoreline change rate rank
+EPR_values <- attrtable[,"EPR"]
 
-hva_cat <- numeric(length(ESI_values ))
+scr_rank <- character(length(EPR_values ))
 
-hva_cat[attrtable[,"EPR"] > 1] <- 1
-hva_cat[attrtable[,"EPR"] > 0.2 & attrtable[,"EPR"] < 1] <- 2
-hva_cat[attrtable[,"EPR"] > -0.2 & attrtable[,"EPR"] < 0.2] <- 3
-hva_cat[attrtable[,"EPR"] > -1 & attrtable[,"EPR"] < -0.20] <- 4
-hva_cat[attrtable[,"EPR"] < -1] <- 5
+scr_rank[attrtable[,"EPR"] >= 1] <- "high"
+scr_rank[attrtable[,"EPR"] > 0.2 & attrtable[,"EPR"] < 1] <- "medium"
+scr_rank[attrtable[,"EPR"] >= -0.2 & attrtable[,"EPR"] <= 0.2] <- "low"
+scr_rank[attrtable[,"EPR"] > -1 & attrtable[,"EPR"] < -0.20] <- "medium"
+scr_rank[attrtable[,"EPR"] <= -1] <- "high"
+
+attrtable$scr_rank <- scr_rank          
 
 
-attrtable$hva_cat <- hva_cat          
+######HVA index setup for absolute value of shoreline change distance variability rank (coef of variation at single transect (uses only the mean at the transect and not the entire population)) NOTE: shoreline change rates could not be used because it is interval data and not ratio data.  Raw distance data is ratio data as long at is is non-negative
+tvar_values <- attrtable[,"CVErasDst"]
 
-esi_hva_table <- cbind(attrtable$Transect, hva_cat)
+tscrv_rank <- character(length(tvar_values  ))
 
-colnames(esi_hva_table) <- c("Transect","hva_cat")
+tscrv_rank[tvar_values >= 1] <- "high"
+tscrv_rank[tvar_values >= 0.5 & tvar_values < 1] <- "medium"
+tscrv_rank[tvar_values < 0.5] <- "low"
 
-write.table(esi_hva_table, file = "ambur_hva_shorechng_data.csv", sep = ",", row.names = FALSE)
+attrtable$tscrv_rank <- tscrv_rank          
+
+#################get spatial variability at a given buffer distance
+ outptsdf <- SpatialPointsDataFrame(out,attrtable)
+
+
+
+out2 <- gBuffer(out, byid=TRUE, id=NULL, width=bufferdist, quadsegs=5, capStyle="ROUND",joinStyle="ROUND", mitreLimit=1.0)
+
+out2a.df <- data.frame(attrtable$Transect)
+colnames(out2a.df) <- "Transect" 
+out2a <- SpatialPolygonsDataFrame(out2,out2a.df)
+projectionString <- proj4string(shapedata) # contains projection info
+proj4string(out2a) <- projectionString
+writeOGR(out2a, ".", "ambur_hva_spatvar_polys", driver="ESRI Shapefile")
+
+
+out3 <- over(out2, outptsdf[,"CVErasDst"], returnList = FALSE, fn = mean)
+
+svar_values <- out3[,1]
+
+
+sscrv_rank <- character(length(svar_values  ))
+
+sscrv_rank[svar_values >= 1] <- "high"
+sscrv_rank[svar_values >= 0.5 & svar_values < 1] <- "medium"
+sscrv_rank[svar_values < 0.5] <- "low"
+
+attrtable$sscrv_rank <- sscrv_rank 
+
+##################HVA Category
+
+hva_cat <- numeric(length(svar_values  ))
+
+hva_cat[scr_rank == "high" & sscrv_rank == "low"] <- 5
+hva_cat[(scr_rank == "high" | scr_rank == "medium") & (sscrv_rank == "medium" | sscrv_rank == "high")] <- 4
+hva_cat[(scr_rank == "medium") & (sscrv_rank == "low" | sscrv_rank == "high")] <- 4
+hva_cat[(scr_rank == "medium") & (tscrv_rank == "medium") & (sscrv_rank == "medium")] <- 3
+hva_cat[(scr_rank == "low") & (tscrv_rank == "medium" | tscrv_rank == "high") & (sscrv_rank == "medium" | sscrv_rank == "high"  )] <- 2
+hva_cat[(scr_rank == "low") & (tscrv_rank == "low" | tscrv_rank == "medium") & (sscrv_rank == "low" | sscrv_rank == "medium")] <- 1
+
+attrtable$hva_cat <- hva_cat 
+
+sc_hva_table <- cbind(attrtable$Transect, hva_cat,scr_rank,tscrv_rank,sscrv_rank)
+
+colnames(sc_hva_table) <- c("Transect","hva_cat","scr_rank","tscrv_rank","sscrv_rank")
+
+write.table(sc_hva_table, file = "ambur_hva_shorechng_data.csv", sep = ",", row.names = FALSE)
 
 
 ###########
