6cd96e5476b9d086ec8c87470934e7ef71583195
cwjacksonjr (CJ Jackson) <jackson.cwjr@gmail.com>
2013-09-17 05:03:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ambur@221 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ workspace/hva/previous/amburhva/R/ambur.hva.calc.r
@@ -0,0 +1,90 @@
+
+ambur.hva.calc <-
+function(userinput1=1) {
+
+
+# Establish the inputs
+nothing <- userinput1
+
+
+
+require(rgdal)
+require(rgeos)
+require(tcltk)
+
+
+tkmessageBox(message = "Please select the AMBUR_HVA_data_prep directory...")
+getdata <- tk_choose.dir(default = "", caption = "Select directory")
+workingdir <- getdata
+setwd(workingdir)
+
+tkmessageBox(message = "Please select a post-AMBUR analysis transect shapefile generated from ambur.statshape function...")
+ filters <- matrix(c("Shapefile", ".shp"), 1, 2, byrow = TRUE)
+getdata2 <- tk_choose.files(filter = filters,multi = FALSE)
+shapename2 <- gsub(".shp", "", basename(getdata2))
+shapedata2 <- readOGR(getdata2,layer=shapename2)
+attrtable2 <- data.frame(shapedata2)
+
+write.table(attrtable2, file = "ambur_hva__data.csv", sep = ",", row.names = FALSE)
+
+
+
+
+
+
+time.stamp1 <- as.character(Sys.time())
+
+time.stamp2 <- gsub("[:]", "_", time.stamp1)
+
+#dir.create("AMBUR_HVA_data_prep", showWarnings=FALSE)
+#setwd("AMBUR_HVA_data_prep")
+
+#######################
+
+
+multmerge = function(mypath){
+filenames=list.files(path=mypath, full.names=TRUE, pattern = "*.csv" )
+datalist = lapply(filenames, function(x){read.csv(file=x,header=T)})
+Reduce(function(x,y) {merge(x,y,by.x = "Transect", by.y = "Transect", all= TRUE, sort=TRUE)}, datalist)
+ }
+
+ #list.files(path=getdata, full.names=TRUE, pattern = "*.csv" )
+# lf <- list.files(path=getdata, full.names=TRUE)
+# lapply(lf, function(x){read.csv(file=x,header=T)})
+ 
+ merge.tab <- multmerge(getdata)
+
+merged.files <- list.files(path=getdata, full.names=FALSE, pattern = "*.csv" )[-1]
+
+rename1 <- gsub("ambur_hva_", "", merged.files)
+rename2 <- gsub("data.csv", "cat", rename1)
+
+merge.tab2 <- as.data.frame(merge.tab)    
+
+colnames(merge.tab2)[(length(colnames(merge.tab2)) - (length(rename2)-1)):(length(colnames(merge.tab2)))] <- rename2
+
+hva_columns <- (length(colnames(merge.tab2)) - (length(rename2)-1)):(length(colnames(merge.tab2)))
+
+merge.tab2[,hva_columns][merge.tab2[,hva_columns]==0] <- NA
+
+hva.product <- apply(merge.tab2[,hva_columns],1,prod,na.rm=TRUE)
+
+test.variables <-  ifelse(is.na(merge.tab2[,hva_columns]) == TRUE, 0, 1)      ###get number of variables present
+n.variables <- apply(test.variables,1,sum)
+
+hva.calc <- sqrt(hva.product/n.variables)
+
+merge.tab2$hva_calc <- hva.calc
+
+row.names(merge.tab2) <- as.numeric(row.names(attrtable2))
+
+lns.output <- SpatialLinesDataFrame(shapedata2,merge.tab2)
+
+    projectionString <- proj4string(shapedata2) # contains projection info
+
+  proj4string(lns.output) <- projectionString
+
+writeOGR(lns.output, ".", "ambur_hva_calculations", driver="ESRI Shapefile")
+
+
+}
