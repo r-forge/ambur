f8c2d7cdfafb8b6cd1c682af4841e72fcdf268f7
cwjacksonjr (CJ Jackson) <jackson.cwjr@gmail.com>
2013-09-24 01:41:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ambur@238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ workspace/merge.csv.files.r
@@ -0,0 +1,26 @@
+merge.csv.files <-
+function(userinput1=1) {
+
+
+# Establish the inputs
+nothing <- userinput1
+
+
+
+require(rgdal)
+require(rgeos)
+require(tcltk)
+
+
+tkmessageBox(message = "Please select the directory with the CSV files...")
+getdata <- tk_choose.dir(default = "", caption = "Select directory")
+workingdir <- getdata
+setwd(workingdir)
+
+
+filenames <- list.files(path = getdata)
+merge.tab <- do.call("rbind", lapply(filenames, read.csv, header = TRUE))
+
+write.table(merge.tab, file = "merged_csv_data.csv", sep = ",", row.names = FALSE)
+
+}
