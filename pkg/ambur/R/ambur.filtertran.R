1a7f91b18df61818a1c961b8881dc1a8d92c239f
cwjacksonjr (CJ Jackson) <jackson.cwjr@gmail.com>
2022-03-30 12:51:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ambur@290 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ambur/R/ambur.filtertran.R
+++ pkg/ambur/R/ambur.filtertran.R
@@ -149,7 +149,7 @@ Pcnt.Complete2 <- paste(Pcnt.Complete," ","%",sep="")
 info <- sprintf("%1.0f percent done", Pcnt.Complete)   
 setTkProgressBar(pb2, i, sprintf("AMBUR: Capture baseline positions (%s)", info), info)
 
-vec[[i]] <- if (sum(int[,i]) != 0) gIntersection(shapedata2[i,], shape.prep3[int[,i],], byid=TRUE)  else 0 }
+vec[[i]] <- if (sum(int[,i]) != 0) gIntersection(shapedata2[i,], shape.prep3[int[,i],])  else 0 }
 
 cond <- lapply(vec, function(x) class(x) != "numeric")
 vec2 <- vec[unlist(cond)]
